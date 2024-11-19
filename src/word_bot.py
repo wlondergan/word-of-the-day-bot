b5f86dd11da81ae22530e1ab1b5041ec473b4b36
wlondergan <whlondergan@gmail.com>
2024-11-18 19:44:50 -0500

    add date-aware checking

--- src/word_bot.py
+++ src/word_bot.py
@@ -1,10 +1,14 @@
 from discord import Client, MessageType, Intents, Message
 import os
 from copy import deepcopy
+from datetime import datetime, timezone
+from zoneinfo import ZoneInfo
 from english_processing import get_word_of_the_day, shortest_available_stem
 
 token = os.environ['TOKEN']
 channel_id = int(os.environ['CHANNEL_ID'])
+eastern_time_info = ZoneInfo('America/New_York')
+utc_info = timezone.utc
 
 EMOJI_ID = 1259346961627086918
 
@@ -12,9 +16,15 @@ intents = Intents.default()
 intents.message_content = True
 
 class WordOfTheDayInfo:
-    def __init__(self, msg_id, user_id):
+    def __init__(self, msg_id, user_id, timecode):
         self.msg_id = msg_id
         self.user_id = user_id
+        self.timecode = timecode
+
+def to_est(date: datetime) -> datetime:
+    if date.tzinfo is None:
+        date.tzinfo = utc_info
+    return date.astimezone(eastern_time_info)
 
 class WordBot(Client):
 
@@ -27,6 +37,12 @@ class WordBot(Client):
                 return self._words[stem]
             else:
                 return stem
+            
+    def _already_posted_on(self, date_time, user) -> bool:
+        for v in self._words.items():
+            if v.user_id == user and date_time.date() == v.timecode.date():
+                return False
+        return True
                 
     async def on_ready(self):
         self._words = {}
@@ -35,11 +51,15 @@ class WordBot(Client):
             if message.author != self.user.id and message.type == MessageType.default:
                 res = self._determine_word_of_the_day(message)
                 if type(res) is str:
-                    self._words[res] = WordOfTheDayInfo(message.id, message.author.id)
+                    timecode = to_est(message.created_at)
+                    if self._already_posted_on(timecode, message.author.id):
+                        continue
+                    else:
+                        self._words[res] = WordOfTheDayInfo(message.id, message.author.id, timecode)
 
     async def on_message(self, message: Message):
 
-        if message.channel.id == channel_id and message.type == MessageType.default: #author's note: surely there's a better way.
+        if message.channel.id == channel_id and message.type == MessageType.default:
             
             #don't bother checking the bot's own messages
             if message.author.id == self.user.id:
@@ -48,7 +68,8 @@ class WordBot(Client):
             res = self._determine_word_of_the_day(message)
 
             if type(res) is str:
-                if res:
+                timestamp = to_est(message.created_at)
+                if res and not self._already_posted_on(timestamp, message.author.id):
                     self._words[res] = WordOfTheDayInfo(message.id, message.author.id)
                     await message.add_reaction(self.get_emoji(EMOJI_ID))
             elif res is not None:
